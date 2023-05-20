f6b63358e685d713c4ba756b6b4c014ffd161d3b
mohamed <mr319242@gmail.com>
2023-05-20 17:35:29 +0300

    new upda

--- index.php
+++ index.php
@@ -67,7 +67,7 @@ include 'init.php';
                     <div class="info">
                         <h2>مجموعة بناء للخدمات والمنتجات التربوية والتعليمية:</h2>
                         <p> بلور فكرتها وأسسها الدكتور غسان بن محمد الصديقي أوائل عام 1424هـ
-                             لغرض تقديم برامج واستشارات تربوية وخدمات علمية ودورات تدريبية ومقاييس شخصية وفق أسس علمية حديثة في التربية وبناء الإنسان للمساهمة في تحقيق المجتمع الرائد. ومجموعة بناء عبارة عن نطاق واسع من المنتجات التربوية التي تُعنى ببناء الإنسان في جميع أبعاده الشخصية وصولاً به إلى الشخصية السويَّة الفاعلة المؤثِّرة في تنمية مجتمعها وريادته . </p>
+                            لغرض تقديم برامج واستشارات تربوية وخدمات علمية ودورات تدريبية ومقاييس شخصية وفق أسس علمية حديثة في التربية وبناء الإنسان للمساهمة في تحقيق المجتمع الرائد. ومجموعة بناء عبارة عن نطاق واسع من المنتجات التربوية التي تُعنى ببناء الإنسان في جميع أبعاده الشخصية وصولاً به إلى الشخصية السويَّة الفاعلة المؤثِّرة في تنمية مجتمعها وريادته . </p>
                         <a href="about_us" class="btn button"> قراءة المزيد </a>
                     </div>
                 </div>
@@ -80,21 +80,95 @@ include 'init.php';
         </div>
     </div>
 </div>
-
 <!-- END ABOUT US -->
-<!-- END LATEST TRAVEL  -->
-<div class="register_now">
-    <div class="overlay">
-        <div class="container">
-            <div class="data">
-                <h2> سجل الان في هاكم </h2>
-                <p> نعمل على توفير الحلول التشاركية في النقل بين الأفراد لتحصيل المنافع المشتركة
-                </p>
-                <a href="register" class="button btn"> سجل الان </a>
+<!-- START CATEGORIES  -->
+<div class="categories">
+    <div class="container-fluid">
+        <div class="data">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="info">
+                        <img src="uploads/cat1.webp" alt="">
+                        <a href="#"> بناء القيادات </a>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="info">
+                        <img src="uploads/cat1.webp" alt="">
+                        <a href="#"> بناء الشباب الطفل</a>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="info">
+                        <img src="uploads/cat1.webp" alt="">
+                        <a href="#">بناء الجهات</a>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="info">
+                        <img src="uploads/cat1.webp" alt="">
+                        <a href="#"> المقاييس </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </div>
+<!-- END CATEGORIES  -->
+<!-- START SHORT CONATCT -->
+<div class="short_contact">
+    <div class="overlay">
+        <div class="container">
+            <div class="data">
+                <div class="info">
+                    <div>
+                        <h3> هل لديك اي استفسار </h3>
+                    </div>
+                    <div>
+                        <a target="_blank" href="https://wa.me/966554434266" class="btn button"> تواصل معنا الان <i class="fa fa-whatsapp"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END SHORT CONTACT -->
+<!-- START BRANDS  -->
+<div class="brands">
+    <div class="container">
+        <div class="data">
+            <div class="row" style="display: flex; align-items:center">
+            <div class="col-lg-1"></div>
+                <div class="col-lg-2">
+                    <div class="info">
+                        <img src="uploads/logo/1.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="info">
+                    <img src="uploads/logo/2.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="info">
+                    <img src="uploads/logo/3.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="info">
+                    <img src="uploads/logo/4.webp" alt="">
+                    </div>
+                </div>
+                <div class="col-lg-2">
+                    <div class="info">
+                    <img src="uploads/logo/5.webp" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END BRANDS -->
 <?php
 
 include $tem . 'footer.php';
