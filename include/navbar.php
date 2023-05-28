d190b7e414a989d5d055625b3868eb48c6046fc3
mohamed <mr319242@gmail.com>
2023-05-28 14:58:27 +0300

    new update

--- include/navbar.php
+++ include/navbar.php
@@ -1,7 +1,8 @@
 <nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
     <a class="navbar-brand" href="index"> <img style="max-width: 120px;" src="uploads/logo.png" alt=""> </a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="fa fa-bars"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -13,7 +14,8 @@
           <a class="nav-link" id="about_us" href="about_us">عن بناء </a>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+            aria-expanded="false">
             الاقسام
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -22,27 +24,26 @@
             $stmt->execute();
             $allcat = $stmt->fetchAll();
             foreach ($allcat as $cat) {
-            ?>
-              <li><a class="dropdown-item" href="category?id=<?php echo $cat['id']; ?>"> <?php echo $cat['name']; ?> </a></li>
-            <?php
+              ?>
+              <li><a class="dropdown-item" href="category?id=<?php echo $cat['id']; ?>"> <?php echo $cat['name']; ?> </a>
+              </li>
+              <?php
             }
             ?>
 
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link" id="contact_us" href="posts"> المدونة </a>
+          <a class="nav-link" id="posts" href="posts"> المدونة dddd</a>
         </li>
-
         <?php
-
         if (isset($_SESSION['username'])) {
           /*
           $stmt = $connect->prepare("SELECT COUNT(id) as count_id, msg_from, msg_to, travel_id FROM chat WHERE msg_to=? GROUP BY msg_from, travel_id ORDER BY count_id DESC LIMIT 15");
           $stmt->execute(array($_SESSION['username']));*/
           include "message_noti_icons/message.php";
           include "message_noti_icons/notification.php";
-        ?>
+          ?>
           <!--
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -51,11 +52,11 @@
               $stmt->execute(array($_SESSION['username']));
               $user_data = $stmt->fetch();
               if ($user_data['profile_image'] != null && $user_data['profile_image'] != '') {
-              ?>
+                ?>
                 <img src="website_uploads/<?php echo $user_data['profile_image'] ?>" alt="">
               <?php
               } else {
-              ?>
+                ?>
                 <img src="uploads/avatar.gif" alt="">
               <?php
               }
@@ -74,10 +75,10 @@
             </ul>
           </li>
             -->
-        <?php
+          <?php
 
         } else {
-        ?>
+          ?>
           <!--
           <li class="nav-item login">
             <a id="login" class="nav-link" href="login"> دخول </a>
@@ -86,7 +87,7 @@
             <a id="register" class="nav-link" href="register"> حساب جديد </a>
           </li>
         -->
-        <?php
+          <?php
         }
 
         ?>
